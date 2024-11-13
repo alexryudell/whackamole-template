568cedc0b3a7a6728eaaab27bb14f1394a185aeb
alexryudell <alexryudell@gmail.com>
2024-11-13 14:06:09 -0500

    Add files via upload

--- whackamole.py
+++ whackamole.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 
 def main():
@@ -9,12 +10,23 @@ def main():
         mole_image = pygame.image.load("mole.png")
         screen = pygame.display.set_mode((640, 512))
         clock = pygame.time.Clock()
+        moleloc = (0, 0)
         running = True
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if abs(event.pos[0] - moleloc[0]) < 32 and abs(event.pos[1] - moleloc[1]) < 32:
+                        moleloc = (random.randrange(0, 608, 32), random.randrange(0, 480, 32))
+                        screen.blit(mole_image, mole_image.get_rect())
             screen.fill("light green")
+            for i in range(1,16):
+                pygame.draw.line(screen, 'black', (0, i*32), (640, i*32))
+            for i in range(1,20):
+                pygame.draw.line(screen, 'black', (i*32, 0), (i*32, 512))
+
+            screen.blit(mole_image, mole_image.get_rect(topleft=moleloc))
             pygame.display.flip()
             clock.tick(60)
     finally:
